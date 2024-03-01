bae41bc86bf932dc9a32d54b78ed43c65b89804a
Tsvetislav <tsvetislavt99@gmail.com>
2024-03-01 15:30:14 +0200

    Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      exclude: ['**/node_modules/**', '**/sandbox/**'],
+      include: ['**/src/**'],
+    },
+  },
+});
